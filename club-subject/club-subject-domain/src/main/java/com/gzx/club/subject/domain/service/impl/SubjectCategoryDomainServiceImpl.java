613839c6e6defa0bd00968302c9ce2c5d237398f
gzx-wrm <1336416561@qq.com>
2024-06-28 02:27:28 +0800

    update multi-thread implementation method, by using CompletableFuture

--- club-subject/club-subject-domain/src/main/java/com/gzx/club/subject/domain/service/impl/SubjectCategoryDomainServiceImpl.java
+++ club-subject/club-subject-domain/src/main/java/com/gzx/club/subject/domain/service/impl/SubjectCategoryDomainServiceImpl.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.stream.Collectors;
@@ -103,34 +104,24 @@ public class SubjectCategoryDomainServiceImpl implements SubjectCategoryDomainSe
 
         // 用线程池优化查询，注意要在所有线程都结束后才能够返回
         List<FutureTask<Map<Long, List<SubjectLabelBO>>>> futureTasks = new LinkedList<>();
-        subjectCategoryBOs.forEach(subjectCategoryBO1 -> {
-            FutureTask<Map<Long, List<SubjectLabelBO>>> futureTask = new FutureTask<>(() -> queryLabel(subjectCategoryBO1));
-            futureTasks.add(futureTask);
-            threadPoolExecutor.submit(futureTask);
-//            SubjectMapping subjectMapping = new SubjectMapping();
-//            subjectMapping.setCategoryId(subjectCategoryBO1.getId());
-//            subjectMapping.setIsDeleted(IsDeletedFlagEnum.UN_DELETED.code);
-//            List<SubjectMapping> subjectMappingList = subjectMappingService.queryDistinctLabelId(subjectMapping);
-//            if (subjectMappingList.size() <= 0) {
-//                return;
-//            }
-//            List<Long> labelIds = subjectMappingList.stream().map(SubjectMapping::getLabelId).collect(Collectors.toList());
-//            List<SubjectLabel> subjectLabels = subjectLabelService.queryByIds(labelIds);
-//            List<SubjectLabelBO> labelBOList = new LinkedList<>();
-//            subjectLabels.forEach(label -> {
-//                SubjectLabelBO subjectLabelBO = new SubjectLabelBO();
-//                subjectLabelBO.setId(label.getId());
-//                subjectLabelBO.setLabelName(label.getLabelName());
-//                subjectLabelBO.setCategoryId(label.getCategoryId());
-//                subjectLabelBO.setSortNum(label.getSortNum());
-//                labelBOList.add(subjectLabelBO);
-//            });
-//            subjectCategoryBO1.setLabelBOList(labelBOList);
-        });
+//        subjectCategoryBOs.forEach(subjectCategoryBO1 -> {
+//            FutureTask<Map<Long, List<SubjectLabelBO>>> futureTask = new FutureTask<>(() -> queryLabel(subjectCategoryBO1));
+//            futureTasks.add(futureTask);
+//            threadPoolExecutor.submit(futureTask);
+//        });
+
+        /* 使用CompletableFuture任务优化FutureTask，CompletableFuture是FutureTask的一种优化方案，
+        提供了更加丰富的功能，比如支持链式调用、支持回调、非阻塞获取结果等
+         */
+        List<CompletableFuture<Map<Long, List<SubjectLabelBO>>>> futures = subjectCategoryBOs.stream()
+                .map(subjectCategoryBO1 ->
+                        CompletableFuture.supplyAsync(() ->
+                                queryLabel(subjectCategoryBO), threadPoolExecutor))
+                .collect(Collectors.toList());
         HashMap<Long, List<SubjectLabelBO>> map = new HashMap<>();
-        for (FutureTask<Map<Long, List<SubjectLabelBO>>> futureTask : futureTasks) {
+        for (CompletableFuture<Map<Long, List<SubjectLabelBO>>> future: futures) {
             // future get不到，会阻塞住
-            Map<Long, List<SubjectLabelBO>> ret = futureTask.get();
+            Map<Long, List<SubjectLabelBO>> ret = future.get();
             if (CollectionUtils.isEmpty(ret)) {
                 continue;
             }
